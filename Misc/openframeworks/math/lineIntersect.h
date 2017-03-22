1d86cd06f38e3deb46ffeb7eb94749b8095556b0
Bingo <bentleyjobs@gmail.com>
2017-03-22 23:13:05 +0800

    新增平面线段求交测试并求交点算法

--- Misc/openframeworks/math/lineIntersect.h
+++ Misc/openframeworks/math/lineIntersect.h
@@ -35,3 +35,38 @@ bool intersect(ofVec2f aa, ofVec2f bb, ofVec2f cc, ofVec2f dd)
     }
     return true;
 }
+
+// 线段相交测试并求交点
+bool segmentsIntersect(ofVec2f& result, ofVec2f a, ofVec2f b, ofVec2f c, ofVec2f d)
+{
+
+    // 三角形abc 面积的2倍  
+    float area_abc = (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
+
+    // 三角形abd 面积的2倍  
+    float area_abd = (a.x - d.x) * (b.y - d.y) - (a.y - d.y) * (b.x - d.x);
+
+    // 面积符号相同则两点在线段同侧,不相交 (对点在线段上的情况,本例当作不相交处理);  
+    if(area_abc * area_abd >= 0)
+    {
+        return false;
+    }
+
+    // 三角形cda 面积的2倍  
+    float area_cda = (c.x - a.x) * (d.y - a.y) - (c.y - a.y) * (d.x - a.x);
+    // 三角形cdb 面积的2倍  
+    // 注意: 这里有一个小优化.不需要再用公式计算面积,而是通过已知的三个面积加减得出.  
+    float area_cdb = area_cda + area_abc - area_abd;
+    if(area_cda * area_cdb >= 0)
+    {
+        return false;
+    }
+
+    //计算交点坐标  
+    float t = area_cda / (area_abd - area_abc);
+    float dx = t*(b.x - a.x),
+        dy = t*(b.y - a.y);
+
+    result.x = a.x + dx;
+    result.y = a.y + dy;
+}
